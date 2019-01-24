2f15b494b106d859c59f92ea06867f87789ac58b
Russkazkin <russkazkin@gmail.com>
2019-01-24 20:17:52 +0300

    Class updated
    1. Вынес в init() инициализацию приватной переменной с сохранением результатов расчетов
    2. Упростил.
    3. Не совсем понимаю вопрос. Как и указано в задание результат проверки сохраняется в приватном свойстве и доступен через геттер.
    4. Исправил.
    5. Добавил.
    
    
    
    24 янв. 2019 г., в 15:53, Рылов Руслан <rrylov@torbor.ru> написал(а):
    
    1. Конструктор очень сложный получился. Не легче ли использовать init()?
    2. diff() Можно было упростить используя модуль числа.
    3. По-моему результат вычисления вообще нигде не хранится или я ошибаюсь?
    4. namespace выбран не удачный. Хорошо бы бизнес-логику складывать хотя бы в components или modules
    5. Не хватает описания параметров класса

--- PriceDiffControl.php
+++ PriceDiffControl.php
@@ -1,21 +1,28 @@
 <?php
 
-namespace app\models;
+namespace app\modules;
 
 use \yii\base\BaseObject;
 
 /**
  * Class PriceDiffControl
- * @package app\models
+ *
+ * @package app\modules
+ *
+ * @param int $allowedVariation Допустимое отклонение в процентах
+ * @param int $price Текущая цена
+ * @param int|null $oldPrice Предыдущая цена
+ * @param bool $_result Результат проверки цен
+ *
  * Класс контролирует наценку/скидку между текущей ценой и предыдущей
  * Примеры:
- * $model = new PriceDiffControl(15, 120, 100);
+ * $control = new PriceDiffControl(15, 120, 100);
  * var_dump($model->result); //int(0) Так как разница цен 20 процентов, что больше нормы в 15
  *
- * $model = new PriceDiffControl(15, 105, 100);
+ * $control = new PriceDiffControl(15, 105, 100);
  * var_dump($model->result); //int(1) Так как разница цен 5 процентов, что укладывается в норму
  *
- * $model = new PriceDiffControl(15, 105);
+ * $control = new PriceDiffControl(15, 105);
  * var_dump($model->result); //int(1) Старая цена отсутствует, мы сравниваем текущую цену с текущей ценой, что дает нам true на выходе
  */
 
@@ -35,29 +42,35 @@ class PriceDiffControl extends BaseObject
      */
     public function __construct(int $allowedVariation, int $price, int $oldPrice = null, array $config = [])
     {
-        parent::__construct($config);
         $this->allowedVariation = $allowedVariation;
         $this->price = $price;
         $this->oldPrice = $oldPrice ? $oldPrice : $price;
+        parent::__construct($config);
+
+    }
+
+    /**
+     * Метод инициализирует private свойство $_result и сохраняет в него результат проверки цен. Свойство доступно через геттер getResult()
+     */
+    public function init()
+    {
+        parent::init();
         $this->_result = $this->diff($this->price, $this->oldPrice, $this->allowedVariation);
     }
 
     /**
-     * Метод сравнивает два числа,
-     * делит большее на меньшее,
-     * преобразует полученный коэффициент в процент разницы
-     * и сранивает с допустимым значением этой разницы.
-     * Если разница не укладывается в норму, возращает false.
+     * Метод находит процентное изменение
+     * и сранивает с допустимым значением.
+     * Если изменение не укладывается в норму, возращает false.
      * В противном случае true
-     * @param $num1
-     * @param $num2
+     * @param $price
+     * @param $oldPrice
      * @param $allowedVariation
      * @return bool
      */
-    public function diff(int $num1, int $num2, int $allowedVariation) : bool
+    public function diff(int $price, int $oldPrice, int $allowedVariation) : bool
     {
-        $rate = $num1 >= $num2 ? $num1 / $num2 : $num2 / $num1;
-        $diff = ($rate - 1) * 100;
+        $diff = abs(($price - $oldPrice) / $oldPrice) * 100;
         return $diff > $allowedVariation ? false : true;
     }
 
